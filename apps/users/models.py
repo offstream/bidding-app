ea46d584bf6c4ab3ee69020fa3b1f87a1ad44a64
Vanj <evangelinen@apc.edu.ph>
2020-10-08 01:01:43 +0800

    Create UserProfile Model

--- apps/users/models.py
+++ apps/users/models.py
@@ -53,3 +53,16 @@ class User(AbstractBaseUser):
         "Does the user have permissions to view the app `app_label`?"
         # Simplest possible answer: Yes, always
         return True
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
